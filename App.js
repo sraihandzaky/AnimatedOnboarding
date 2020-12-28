8a23aaa28af445d6f1aa15dc9e1b308ab2fb390e
Raihan Dzaky <sraihandzaky@gmail.com>
2020-12-28 15:17:55 +0700

    AnimatedOnboarding

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import OnBoarding from './src/Screens/OnBoarding';
+
+export default function App() {
+  return (
+    <>
+      <OnBoarding />
+    </>
+  );
+}
